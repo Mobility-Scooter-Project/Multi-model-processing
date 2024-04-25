1237554d167801ba1ba798918ec7ab1d84fb0802
justchung <justinjwchung@gmail.com>
2024-04-25 01:30:52 -0700

    created cocoa loss

---
+++ cocoa_loss.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+class CocoaLoss(nn.Module):
+    def __init__(self, tau, lam):
+        super(CocoaLoss, self).__init__()
+        self.tau = tau
+        self.lam = lam
+
+    def forward(self, x_pred_batch, y_pred_batch, label_batch):
+        pos_error = self.__calc_pos_error(x_pred_batch, y_pred_batch)
+        neg_idxs = self.__find_negatives(label_batch)
+        x_neg_error = self.__calc_neg_error(x_pred_batch, neg_idxs)
+        y_neg_error = self.__calc_neg_error(y_pred_batch, neg_idxs)
+        return pos_error + self.lam*(x_neg_error + y_neg_error)
+
+    def __calc_pos_error(self, x_pred_batch, y_pred_batch):
+        pos_error = 0
+        for pose, move in zip(x_pred_batch, y_pred_batch):
+            corr = torch.linalg.matmul(pose, move)
+            corr = torch.div(corr, torch.mul(torch.linalg.vector_norm(x_pred_batch), torch.linalg.vector_norm(y_pred_batch)))
+            corr = torch.sub(1, corr)
+            corr = torch.exp(corr / self.tau)
+            pos_error = torch.add(pos_error, corr)
+        return pos_error
+    
+    def __find_negatives(label_batch):
+        THRESHOLD = 1
+        negatives = []
+        for idx, label_seq in enumerate(label_batch):
+            i = 0
+            for label in label_seq:
+                if not label:
+                    i += 1
+            if i > THRESHOLD:
+                negatives.append(idx)
+        return negatives
+
+    def __calc_neg_error(self, pred_batch, neg_indexs):
+        neg_error = 0
+        i = 0
+        for idx in neg_indexs:
+            non_neg_sequences = [seq for i, seq in enumerate(pred_batch) if i not in neg_indexs]
+            for seq in non_neg_sequences:
+                discrim = torch.linalg.matmul(pred_batch[idx], seq)
+                discrim = torch.div(discrim, torch.mul(torch.linalg.vector_norm(pred_batch[idx]), torch.linalg.vector_norm(seq)))
+                discrim = torch.exp(discrim / self.tau)
+                pos_error = torch.add(pos_error, discrim)
+                i += 1
+        return neg_error / i
--- cocoa_train.py
+++ cocoa_train.py
@@ -4,6 +4,9 @@ import torch
 from torch.utils.data import DataLoader, RandomSampler
 from sequence_dataset import SequenceDataset
 
+# Device agnostic
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
 def match_length(label_arr, pose_arr, move_arr):
     minimum = min(len(label_arr), len(pose_arr), len(move_arr))
     return label_arr[:minimum], pose_arr[:minimum], move_arr[:minimum]
@@ -21,16 +24,16 @@ label_arr = label_arr[::DEFLATION_FACTOR]
 label_arr = change_labels_to_bool(label_arr)
 
 pose_arr = np.loadtxt("aligned_data/041720231030/P002/Yolov7/Front_full.csv",
-                 delimiter=",", dtype=float, skiprows=1)
+                 delimiter=",", dtype="float32", skiprows=1)
 pose_arr = pose_arr[::DEFLATION_FACTOR]
 
 move_arr = np.loadtxt("aligned_data/041720231030/P002/April_17_Run_1.csv",
-                 delimiter=",", usecols=range(1,7), dtype=float, skiprows=1)
+                 delimiter=",", usecols=range(1,7), dtype="float32", skiprows=1)
 
 label_arr, pose_arr, move_arr = match_length(label_arr, pose_arr, move_arr)
 
 RANDOM_SEED = 42
-FRAME_LENGTH = 2
+SEQUENCE_LENGTH = 2
 BATCH_SIZE = 5
 POSE_N_FEATURES = 18
 MOVE_N_FEATURES = 6
@@ -38,17 +41,17 @@ TEST_SIZE = 0.15
 
 label_dataset = SequenceDataset( 
     label_arr,
-    sequence_length=FRAME_LENGTH
+    sequence_length=SEQUENCE_LENGTH
 )
 
 pose_dataset = SequenceDataset( 
     pose_arr,
-    sequence_length=FRAME_LENGTH
+    sequence_length=SEQUENCE_LENGTH
 )
 
 move_dataset = SequenceDataset( 
     move_arr,
-    sequence_length=FRAME_LENGTH
+    sequence_length=SEQUENCE_LENGTH
 )
 
 # Create train-test split
@@ -61,29 +64,8 @@ pose_train_dataset, pose_test_dataset = \
 move_train_dataset, move_test_dataset = \
     train_test_split(move_dataset, test_size=TEST_SIZE, random_state=RANDOM_SEED)
 
-def find_negatives(label_batch):
-    THRESHOLD = 10
-    negatives = []
-    for idx, label_seq in enumerate(label_batch):
-        i = 0
-        for label in label_seq:
-            if not label:
-                i += 1
-        if i > THRESHOLD:
-            negatives.append(idx)
-    return negatives
 
-def create_neg_pairs(batch, neg_indexs):
-    pairs = []
-    batch_list = list(batch)
-    for idx in neg_indexs:
-        non_neg_sequences = [seq for i, seq in enumerate(batch) if i not in neg_indexs]
-        for seq in non_neg_sequences:
-            pairs.append((batch_list[idx], seq))
-    return pairs
-
-
-def train_model(label_train_dataset, label_test_dataset, pose_train_dataset, pose_test_dataset, 
+def train_model(model, label_train_dataset, label_test_dataset, pose_train_dataset, pose_test_dataset, 
                 move_train_dataset, move_test_dataset):
     G = torch.Generator()
     G.manual_seed(RANDOM_SEED)
@@ -99,13 +81,22 @@ def train_model(label_train_dataset, label_test_dataset, pose_train_dataset, pos
     move_train_loader = DataLoader(move_train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler_save)
 
     for label_batch, pose_batch, move_batch in zip(iter(label_train_loader), iter(pose_train_loader), iter(move_train_loader)):
-        print(label_batch)
-        print(pose_batch)
-        print(move_batch)
+        pred_pose_batch = []
+        pred_move_batch = []
+        for pose_true, move_true in zip(pose_batch, move_batch):
+            pose_true, move_true = pose_true.to(device), move_true.to(device)
+            pose_pred, move_pred = model(pose_true, move_true)
+            pred_pose_batch.append(pose_pred)
+            pred_move_batch.append(move_pred)
+        # loss = loss_fn(pred_pose_batch, pred_move_batch, label_batch)
+
         #TODO: Create loss function and implement training
+        # loss = loss / accum_iter 
+        # optimizer.step()
+        # optimizer.zero_grad()
+        
 
-
-# train_model(label_train_dataset, label_test_dataset, pose_train_dataset, pose_test_dataset, 
+# train_model(None, label_train_dataset, label_test_dataset, pose_train_dataset, pose_test_dataset, 
 #                 move_train_dataset, move_test_dataset)
 
     
