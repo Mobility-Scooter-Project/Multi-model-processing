92416af2214c1404fc58c1243fbfa2af1a5fa90b
CrystarRay <crystar151129@gmail.com>
2024-05-08 12:49:39 -0700

    add fetch data files

---
+++ config.py
@@ -0,0 +1,3 @@
+# Current implementation deflates the number of pose to match the number of move points
+# Another implementation may be to inflate the number of move sequences to match number of pose sequences
+DEFLATION_FACTOR = 18 # Video frames to movement frames ratio is 18:1
